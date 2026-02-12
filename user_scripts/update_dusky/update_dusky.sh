1cf59a6e881fed6bd54d2bab998b0130c3a59675
dusk <dusk1@myyahoo.com>
2026-02-12 15:20:18 +0530

    wayclick

--- user_scripts/update_dusky/update_dusky.sh
+++ user_scripts/update_dusky/update_dusky.sh
@@ -260,7 +260,7 @@ declare -ra UPDATE_SEQUENCE=(
 #    "U | 082_config_bat_notify.sh --default"
     "U | 083_set_thunar_terminal_kitty.sh"
     "U | 084_package_removal.sh --auto"
-    "U | 085_wayclick_reset.sh"
+#    "U | 085_wayclick_reset.sh"
 #    "U | 086_generate_colorfiles_for_current_wallpaer.sh"
     "U | 087_hypr_custom_config_setup.sh"
     "U | 088_hyprctl_reload.sh"
