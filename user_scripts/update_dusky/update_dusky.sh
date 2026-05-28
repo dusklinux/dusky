53818cff6b2ef46ee51672cbe67dfa12cbc787a6
dusk <dusk1@myyahoo.com>
2026-05-28 10:43:39 +0530

    uwsm gpu setter in archiso

--- user_scripts/update_dusky/update_dusky.sh
+++ user_scripts/update_dusky/update_dusky.sh
@@ -194,6 +194,7 @@ declare -ra UPDATE_SEQUENCE=(
 
 #    "U | 002_pre_generated_colors.sh"
 #    "U | 003_network_connect.sh"
+    "U | 005_hypr_custom_config_setup.sh"
     "U | 005_hypr_custom_config_setup.sh --force --workspace_rules"
     "U | 010_package_removal.sh --auto"
 
