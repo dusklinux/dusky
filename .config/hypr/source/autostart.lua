9cd8c9780ccf3a53ea77426db1ced62bb082a441
dusk <dusklinux@users.noreply.github.com>
2026-07-12 23:38:14 +0530

    fixed clipboard

--- .config/hypr/source/autostart.lua
+++ .config/hypr/source/autostart.lua
@@ -29,7 +29,7 @@ hl.on("hyprland.start", function()
     hl.exec_cmd("sh -c '. $HOME/.config/dusky/settings/cliphist_db_env && exec wl-paste --type image --watch cliphist store'")
 
     -- persist clipboard ---
-    hl.exec_cmd("wl-clip-persist --clipboard regular")
+    -- hl.exec_cmd("wl-clip-persist --clipboard regular")
 
 
 end)
