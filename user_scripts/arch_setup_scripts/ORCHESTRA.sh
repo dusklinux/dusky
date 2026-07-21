2cc1e4822caae15cd601cb610be28d3be110954c
dusk <dusklinux@users.noreply.github.com>
2026-07-21 12:48:44 +0530

    package installer

--- user_scripts/arch_setup_scripts/ORCHESTRA.sh
+++ user_scripts/arch_setup_scripts/ORCHESTRA.sh
@@ -68,7 +68,7 @@ INSTALL_SEQUENCE=(
     "S | 050_pacman_config.sh --auto"
     "S | 051_pacman_hooks.sh --auto"
     "S | 055_pacman_reflector.sh"
-    "S | 060_package_installation.py"
+    "S | 060_package_installation.sh"
     "U | 065_enabling_user_services.sh"
     "S | 068_ufw_firewall.sh"
     "S | 070_openssh_setup.sh --auto"
