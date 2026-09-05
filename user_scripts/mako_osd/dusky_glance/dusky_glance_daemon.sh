a7f682dfdc367783c0d790bc830523e4c4251247
dusk <dusk1@myyahoo.com>
2026-09-05 14:42:34 +0530

    network meter reliablity

--- user_scripts/mako_osd/dusky_glance/dusky_glance_daemon.sh
+++ user_scripts/mako_osd/dusky_glance/dusky_glance_daemon.sh
@@ -732,15 +732,41 @@ case "$MODE" in
             printf "" > "$HEARTBEAT_FILE"
             if [[ -r "$DAEMON_PID_FILE" ]]; then
                 read -r d_pid < "$DAEMON_PID_FILE" 2>/dev/null || d_pid=""
-                if [[ -n "$d_pid" ]] && kill -0 "$d_pid" 2>/dev/null; then
-                    kill -USR1 "$d_pid" 2>/dev/null || true
-                fi
+                case "$d_pid" in
+                    ""|*[!0-9]*) ;;
+                    *)
+                        if kill -0 "$d_pid" 2>/dev/null; then
+                            if exec {_gfd}< "/proc/$d_pid/cmdline" 2>/dev/null; then
+                                IFS= read -r -d '' _g1 <&"$_gfd" 2>/dev/null || _g1=""
+                                IFS= read -r -d '' _g2 <&"$_gfd" 2>/dev/null || _g2=""
+                                exec {_gfd}<&- 2>/dev/null
+                                [[ "$_g2" == *network_meter_daemon* ]] && kill -USR1 "$d_pid" 2>/dev/null
+                            fi
+                        fi
+                        ;;
+                esac
             fi
         fi
         
         while true; do
             [[ -d "$STATE_DIR" ]] && printf "" > "$HEARTBEAT_FILE"
-
+            if [[ -r "$DAEMON_PID_FILE" ]]; then
+                read -r _gp < "$DAEMON_PID_FILE" 2>/dev/null || _gp=""
+                case "$_gp" in
+                    ""|*[!0-9]*) ;;
+                    *)
+                        if kill -0 "$_gp" 2>/dev/null; then
+                            if exec {_gfd}< "/proc/$_gp/cmdline" 2>/dev/null; then
+                                IFS= read -r -d '' _g1 <&"$_gfd" 2>/dev/null || _g1=""
+                                IFS= read -r -d '' _g2 <&"$_gfd" 2>/dev/null || _g2=""
+                                exec {_gfd}<&- 2>/dev/null
+                                [[ "$_g2" == *network_meter_daemon* ]] && kill -USR1 "$_gp" 2>/dev/null
+                            fi
+                        fi
+                        ;;
+                esac
+                unset _gp _g1 _g2 _gfd
+            fi
             if [[ -r "$STATE_FILE" ]]; then
                 unit=""; up=""; down=""
                 for ((_rt=0; _rt<5; _rt++)); do
